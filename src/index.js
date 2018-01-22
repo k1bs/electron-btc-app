e48bd61614e72d31c33d12f36cc41a55fa87dbed
Alex Kibler <alexkibler@me.com>
2018-01-22 10:59:43 -0500

    add shit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
+const fetch = require('cross-fetch')
 
 const notifyBtn = document.getElementById('notify-button')
 
 notifyBtn.addEventListener('click', (event) => {
   const modalPath = path.join('file://', __dirname, 'add.html')
-  let win = new BrowserWindow({width: 400, height: 200})
+  let win = new BrowserWindow({frame: false, transparent: true, alwaysOnTop: true, width: 400, height: 200})
   win.on('close', () => {
     win = null
   })
