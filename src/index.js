470ef1e243888cc006c22c992c2045527626a443
Alex Kibler <alexkibler@me.com>
2018-01-22 11:21:04 -0500

    add fetch

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ const BrowserWindow = electron.remote.BrowserWindow
 const fetch = require('cross-fetch')
 
 const notifyBtn = document.getElementById('notify-button')
+const price = document.querySelector('h1')
+const targetPrice = document.getElementById('target-price')
+
+function getBTC () {
+  fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+    .then(res => res.json())
+    .then(json => {
+      const cryptos = json.BTC.USD
+      price.innerHTML = `$ ${cryptos.toLocaleString('en')}`
+    })
+}
+
+getBTC()
+
+setInterval(getBTC, 30000)
 
 notifyBtn.addEventListener('click', (event) => {
   const modalPath = path.join('file://', __dirname, 'add.html')
