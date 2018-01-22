3ce7a44591e071e927a35483ae923f346295a26d
Alex Kibler <alexkibler@me.com>
2018-01-22 10:48:42 -0500

    move js files, add basic js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.remote.BrowserWindow
+
+const notifyBtn = document.getElementById('notify-button')
+
+notifyBtn.addEventListener('click', (event) => {
+  const modalPath = path.join('file://', __dirname, 'add.html')
+  let win = new BrowserWindow({width: 400, height: 200})
+  win.on('close', () => {
+    win = null
+  })
+  win.loadURL(modalPath)
+  win.show()
+})
